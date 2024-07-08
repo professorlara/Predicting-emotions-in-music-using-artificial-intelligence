295a0f7c9e8e2ca36cb4c8fc300321b21293cd93
professorlara <126965783+professorlara@users.noreply.github.com>
2024-07-08 13:51:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ nltk.download('averaged_perceptron_tagger')
 from textblob import TextBlob
 from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
-import numpy as 
+import numpy as np
 
 # Define the functions
 def wordcount(text):
